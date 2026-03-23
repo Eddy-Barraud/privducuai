65b33eb0ea946a59caf16ca7756f4903694784dc
Eddy Barraud <eddy.barraud@outlook.fr>
2026-03-23 16:16:11 +0100

    Initial Commit

---
+++ Privducai/PrivducaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivducaiApp.swift
+//  Privducai
+//
+//  Created by Eddy Barraud on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PrivducaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
